a3fa9e5fe2d41430b9ed7aad51a85da9b8a86cfd
mrudangm <72047084+mrudangm@users.noreply.github.com>
2024-04-15 22:33:42 -0500

    Update Leaflet_Material.f

--- Healthy_QuasiStatic/Leaflet_Material.f
+++ Healthy_QuasiStatic/Leaflet_Material.f
@@ -18,16 +18,7 @@ C
       if (cmname(1:11) .eq. 'VUANISO_HGO') then
          call VUANISOHYPER_INVHGO(sInvariant, uDev, zeta, nFiber, ninv,
      $     duDi, d2uDiDi, nb, numprops, props)
-      else if(cmname(1:14) .eq. 'VUANISO_INVISO') then
-         call VUANISOHYPER_INVISO(sInvariant, uDev, zeta, nFiber, ninv,
-     $     duDi, d2uDiDi, nb, numprops, props)
-      else if(cmname(1:17) .eq. 'VUANISO_FUNGINV44') then
-         call VUANISOHYPER_FUNGINV44(sInvariant, uDev, zeta, nFiber,
-     $ ninv, duDi, d2uDiDi, nb, numprops, props)
-      else if(cmname(1:17) .eq. 'VUANISO_FUNGINV45') then
-         call VUANISOHYPER_FUNGINV45(sInvariant, uDev, zeta, nFiber, 
-     $ ninv, duDi, d2uDiDi, nb, numprops, props)
-	  else if(cmname(1:11) .eq. 'VUANISO_ISO') then
+      else if(cmname(1:11) .eq. 'VUANISO_ISO') then
          call VUANISOHYPER_ISO(sInvariant, uDev, zeta, nFiber, 
      $ ninv, duDi, d2uDiDi, nb, numprops, props)
       else
@@ -186,7 +177,7 @@ c     compressible case
           Dinv = one / props(2)
 c		  print *,props(2)
           det = ainv (kb, index_J)
-		  ui1(kb,index_J) = two*Dinv * (det - one)
+          ui1(kb,index_J) = two*Dinv * (det - one)
           ui2(kb,indx(index_J,index_J))= two*Dinv
 c          ui1 (kb, index_J) = Dinv * (det - one / det)
 c          ui2 (kb, indx (index_J, index_J)) = Dinv * (one + one / det / det)
